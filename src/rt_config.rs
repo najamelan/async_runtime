d39afda905468c7174c1b61d2491f41a443990c6
Naja Melan <najamelan@autistici.org>
2019-08-17 15:10:15 +0200

    add Copy to RtConfig

--- src/rt_config.rs
+++ src/rt_config.rs
@@ -1,6 +1,6 @@
 /// The configuration for wich executor that should be used on this thread.
 //
-#[ derive( Debug, Clone, Hash, PartialEq, Eq ) ]
+#[ derive( Debug, Copy, Clone, Hash, PartialEq, Eq ) ]
 //
 pub enum RtConfig
 {
